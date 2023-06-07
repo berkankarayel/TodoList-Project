5b818e1ff8f44bf849041468dfca0c00ffba8180
berkankarayel <berkankarayel.147@gmail.com>
2023-06-07 12:00:05 +0300

    .

---
+++ app.js
@@ -0,0 +1,11 @@
+//Tüm Elementleri Seçmek
+
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const clearButton = document.querySelector("#clearButton");
+const filterInput = document.querySelector("#todoSearch");
+
+let todos = [];
